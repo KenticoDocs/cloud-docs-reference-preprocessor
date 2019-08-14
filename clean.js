b76fdea9e644b3275d6a6238576c028ccbe8b60c
Enngage <ennspassion@gmail.com>
2019-08-14 09:48:29 +0200

    Reduces clean complexity

--- clean.js
+++ clean.js
@@ -2,18 +2,21 @@ var fs = require('fs');
 
 const folderToClean = './dist';
 
+function processDeletePath(path) {
+  fs.readdirSync(path).forEach(function(file, index){
+    var curPath = path + '/' + file;
+
+    if (fs.lstatSync(curPath).isDirectory()) { // recurse
+      deleteFolderRecursive(curPath);
+    } else { // delete file
+      fs.unlinkSync(curPath);
+    }
+  });
+}
+
 function deleteFolderRecursive(path) {
   if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
-    fs.readdirSync(path).forEach(function(file, index){
-      var curPath = path + '/' + file;
-
-      if (fs.lstatSync(curPath).isDirectory()) { // recurse
-        deleteFolderRecursive(curPath);
-      } else { // delete file
-        fs.unlinkSync(curPath);
-      }
-    });
-
+    processDeletePath(path);
     console.log(`Deleting directory '${path}'`);
     fs.rmdirSync(path);
   }
